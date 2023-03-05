570b2801f664d81e8b967497255042839b857182
James Parker <parkerman23@gmail.com>
2023-03-05 17:52:13 +0000

    tweaked

--- catnapsite/settings.py
+++ catnapsite/settings.py
@@ -17,7 +17,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-!tc&-swutzm+s53!zl&6u4$2ilan-b52()c&atc@d8!nyn1z4_'
+SECRET_KEY = config('SECRET_KEY')
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
