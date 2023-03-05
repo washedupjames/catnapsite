e689ecfc039f5a6554f3eb4c5b918788bbf933f5
James Parker <parkerman23@gmail.com>
2023-03-05 16:18:47 +0000

    tweaked

--- catnapsite/settings.py
+++ catnapsite/settings.py
@@ -76,7 +76,7 @@ WSGI_APPLICATION = 'catnapsite.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': OS.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
